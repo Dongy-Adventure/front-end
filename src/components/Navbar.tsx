ca63f81d920687c98dc59ba58d35b2bca33d0b7a
D33102 <dalai2547@gmail.com>
2025-01-24 10:37:12 +0700

    feat: tailwind

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,21 +9,21 @@ export default function Navbar() {
   const [active, setActive] = useState<Page | null>(null);
 
   return (
-    <nav className="bg-blue-800 w-80 grid grid-cols-3 place-items-center rounded-lg p-1">
-      <Icon
-        icon="mdi-light:home"
-        onClick={() => setActive('Home')}
-        className={cn(
-          'w-8 h-8 rounded-md',
-          active === 'Home' && 'bg-white text-blue-700'
-        )}
-      />
+    <nav className="bg-project-blue w-80 grid grid-cols-3 place-items-center rounded-lg p-1">
       <Icon
         icon="mdi-light:format-list-checks"
         onClick={() => setActive('Order')}
         className={cn(
           'w-8 h-8 rounded-md',
-          active === 'Order' && 'bg-white text-blue-700'
+          active === 'Order' && 'bg-white text-project-blue'
+        )}
+      />
+      <Icon
+        icon="mdi-light:home"
+        onClick={() => setActive('Home')}
+        className={cn(
+          'w-8 h-8 rounded-md',
+          active === 'Home' && 'bg-white text-project-blue'
         )}
       />
       <Icon
@@ -31,7 +31,7 @@ export default function Navbar() {
         onClick={() => setActive('Profile')}
         className={cn(
           'w-8 h-8 rounded-md',
-          active === 'Profile' && 'bg-white text-blue-700'
+          active === 'Profile' && 'bg-white text-project-blue'
         )}
       />
     </nav>
