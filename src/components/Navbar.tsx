a29b7e18c6152b9470a845da98008165d05386ab
D33102 <dalai2547@gmail.com>
2025-01-24 10:15:58 +0700

    feat: nav bar incomplete

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { Icon } from '@iconify/react';
+import { useState } from 'react';
+import { cn } from '@/lib/utils';
+
+type Page = 'Home' | 'Order' | 'Profile';
+
+export default function Navbar() {
+  const [active, setActive] = useState<Page | null>(null);
+
+  return (
+    <nav className="bg-blue-800 w-80 grid grid-cols-3 place-items-center rounded-lg p-1">
+      <Icon
+        icon="mdi-light:home"
+        onClick={() => setActive('Home')}
+        className={cn(
+          'w-8 h-8 rounded-md',
+          active === 'Home' && 'bg-white text-blue-700'
+        )}
+      />
+      <Icon
+        icon="mdi-light:format-list-checks"
+        onClick={() => setActive('Order')}
+        className={cn(
+          'w-8 h-8 rounded-md',
+          active === 'Order' && 'bg-white text-blue-700'
+        )}
+      />
+      <Icon
+        icon="mdi-light:account"
+        onClick={() => setActive('Profile')}
+        className={cn(
+          'w-8 h-8 rounded-md',
+          active === 'Profile' && 'bg-white text-blue-700'
+        )}
+      />
+    </nav>
+  );
+}
