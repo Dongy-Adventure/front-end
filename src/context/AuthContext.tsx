5207a96acb075dd68497d751c6d13cfee22d1f9b
D33102 <dalai2547@gmail.com>
2025-01-25 14:23:48 +0700

    feat: add some more file

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,11 +1,12 @@
 'use client';
 import { Buyer, Seller } from '@/types/user';
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/router';
 import React, {
   createContext,
   ReactNode,
   useCallback,
   useContext,
-  useEffect,
   useState,
 } from 'react';
 
@@ -18,3 +19,35 @@ interface IAuthContext {
 const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
 export const useAuth = () => useContext(AuthContext);
+
+const AuthProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  const [user, setUser] = useState<Buyer | Seller | null>(null);
+  const path = usePathname();
+  const router = useRouter();
+  const [isReady, setIsReady] = useState<boolean>(false);
+
+  const resetContext = useCallback(async () => {
+    const userData = null; // implement API
+    if (userData) {
+      localStorage.setItem('user', JSON.stringify(userData));
+      setUser(userData);
+    }
+  }, []);
+
+  const logout = useCallback(() => {
+    localStorage.clear();
+    window.location.href = '/';
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ user, resetContext, logout }}
+    ></AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
