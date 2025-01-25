4cb02f1abd8b574403c7f9d166df7f90e4889f06
D33102 <dalai2547@gmail.com>
2025-01-25 15:11:56 +0700

    chore: add auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Athiti } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import AuthProvider from '@/context/AuthContext';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -36,7 +37,7 @@ export default function RootLayout({
         className={`${athiti.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Toaster />
-        {children}
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
