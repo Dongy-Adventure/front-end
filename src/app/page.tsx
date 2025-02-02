79f340c0087693232b8500672fbea717879a00f8
Korn <kornmio08@gmail.com>
2025-02-02 20:31:45 +0700

    Transaction History Version 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Navbar from '@/components/Navbar';
+import ProfileForm from '@/components/ProfileForm';
+import Spinner from '@/components/Spinner';
+import TransactionHistory from './TransactionHistory';
 
 export default function Home() {
-  return <main className="flex justify-center"></main>;
+  return <main className="flex justify-center">
+    <TransactionHistory />
+  </main>;
 }
