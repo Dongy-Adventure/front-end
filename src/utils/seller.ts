b0ea84e9c10683b754757cbfe1d23428f863da8f
petchluvsyou <petchluvsyou@outlook.com>
2025-02-10 00:40:27 +0700

    feat: get data from api & update profile

--- src/utils/seller.ts
+++ src/utils/seller.ts
@@ -1,3 +1,4 @@
+import { getUserId } from './auth';
 import { apiClient } from './axios';
 
 export const createSeller = async (
@@ -5,7 +6,8 @@ export const createSeller = async (
   surname: string,
   payment: string,
   password: string,
-  username: string
+  username: string,
+  phoneNumber: string,
 ): Promise<boolean | null> => {
   try {
     const res = await apiClient.post('/seller', {
@@ -14,6 +16,36 @@ export const createSeller = async (
       payment: payment,
       password: password,
       username: username,
+      phoneNumber: phoneNumber,
+    });
+    if (!res.data.success) return false;
+
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
+
+export const updateSeller = async (
+  name: string,
+  surname: string,
+  phoneNumber: string,
+  address: string,
+): Promise<boolean | null> => {
+  try {
+    console.log("Received tel:", phoneNumber);
+    console.log("Received address:", address);
+    const id = await getUserId();
+    const res = await apiClient.put(`/seller/${id}`, {
+      name: name,
+      surname: surname,
+      phoneNumber: phoneNumber,
+      address: address,
+    }, {
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzkxMjI1OTV9.NS2qzjsUO9iTPa-M1NZnJlco_DAOhJCvUKsojAF3KHs`,
+      },
     });
     if (!res.data.success) return false;
 
