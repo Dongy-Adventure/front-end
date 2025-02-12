59200cf102f3afdd8d720027de330c6a910d828b
D33102 <dalai2547@gmail.com>
2025-02-12 15:22:25 +0700

    chore: type error

--- src/types/user.ts
+++ src/types/user.ts
@@ -18,7 +18,7 @@ export type Seller = {
   score: number;
   sellerID: string;
   surname: string;
-  transaction: Transaction;
+  transaction: Transaction[];
   username: string;
   userType: 'seller';
 };
