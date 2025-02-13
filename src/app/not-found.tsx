f52b1418917d4b320344f14f5106ab0168560cac
Sirapatch <110986784+petchluvsyou@users.noreply.github.com>
2025-02-13 19:40:10 +0700

    Not found page (#31)
    * feat: not found page, tailwind color config
    
    * fix: text color

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,29 @@
+import Image from 'next/image';
+import notFound from '@/../public/404.png';
+import Link from 'next/link';
+
 export default function NotFound() {
-  return <p>Not Found</p>;
+  return (
+    <div className="flex flex-col items-center py-8 lg:py-16 px:8 lg:px-24 w-full h-auto bg-project-secondary gap-4">
+      <Image
+        src={notFound}
+        alt="404"
+        width={874}
+        height={376}
+        className="w-1/2 min-w-72 h-auto pb-2"
+      />
+      <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">
+        That Page Cant Be Found
+      </h1>
+      <p className="text-sm md:text-md lg:text-lg xl:text-xl font-light text-center px-12 leading-tight text-gray-600">
+        It looks like nothing was found at this location. Maybe try to search
+        for what you are looking for?
+      </p>
+      <Link href="./">
+        <button className="mt-6 w-40 h-12 bg-project-dark rounded-xl text-white font-semibold">
+          Go To Homepage
+        </button>
+      </Link>
+    </div>
+  );
 }
