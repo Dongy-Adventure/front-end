304b68674d7b5f72b8ac45ffdb6cf2c500e4c312
petchluvsyou <petchluvsyou@outlook.com>
2025-02-15 22:11:50 +0700

    fix: seller rating on product page

--- src/components/ProductSellerCard.tsx
+++ src/components/ProductSellerCard.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import placeholder from '@/../public/placeholder2.jpg';
 import { Seller } from '@/types/user';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 export default function ProductSellerCard({ seller }: { seller: Seller }) {
   return (
@@ -12,7 +13,22 @@ export default function ProductSellerCard({ seller }: { seller: Seller }) {
       />
       <div className="flex-col gap-0">
         <p className="font-semibold">{seller.username}</p>
-        <p>score: {seller.score}</p>
+        <div className="flex">
+          {[2, 4, 6, 8, 10].map((threshold, index) => (
+            <Icon
+              key={index}
+              className="-ml-1.5"
+              icon={
+                seller.score >= threshold
+                  ? 'material-symbols-light:star'
+                  : 'material-symbols-light:star-outline'
+              }
+              width="20"
+              height="20"
+            />
+          ))}
+          <p className="-mt-0.5"> | 96 Ratings</p>
+        </div>
       </div>
     </div>
   );
