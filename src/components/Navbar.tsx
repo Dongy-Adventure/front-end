82366b75674be3a04d879dccf9e81a7e99384228
Sirapatch <110986784+petchluvsyou@users.noreply.github.com>
2025-02-17 14:32:50 +0700

    Navbar (#40)
    * fix: register redirects to profile edit
    
    * fix: navbar design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 import logo from '@/../public/logo.png';
 import Image from 'next/image';
+import { useAuth } from '@/context/AuthContext';
 
 type Page = 'Home' | 'Order' | 'Profile';
 
@@ -39,9 +40,17 @@ const navs: NavComponent[] = [
 export default function Navbar() {
   const router = useRouter();
   const [active, setActive] = useState<Page | null>(null);
+  const [query, setQuery] = useState('');
+  const user = useAuth();
+
+  const handleSearch = () => {
+    if (query != '') {
+      router.push(`/search?q=${query}`);
+    }
+  };
 
   return (
-    <nav className="z-50 flex justify-stretch p-1 bg-white shadow-md fixed md:top-0 bottom-4 md:bottom-auto md:left-0 left-1/2 transform -translate-x-1/2 md:translate-x-0 w-4/5 md:w-screen">
+    <nav className="z-50 flex justify-stretch p-2 bg-white shadow-md fixed md:top-0 bottom-4 md:bottom-auto md:left-0 left-1/2 transform -translate-x-1/2 md:translate-x-0 w-4/5 md:w-screen">
       <div className="flex align-center justify-center w-full">
         <div className="w-10 h-10">
           <Image
@@ -54,8 +63,46 @@ export default function Navbar() {
           TarsMarketplace
         </p>
       </div>
-      <div className="flex align-center justify-center w-full">
-        <div className="bg-gray-200 rounded-lg w-full my-1"></div>
+      <div className="hidden md:flex items-center justify-center w-full bg-gray-200 rounded-lg px-2">
+        <input
+          className="bg-gray-200 flex-grow rounded-lg px-2 outline-none"
+          value={query}
+          placeholder="Search Anything"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setQuery(e.target.value)
+          }
+        />
+        <Icon
+          icon="material-symbols:search"
+          width="20"
+          height="20"
+          className="text-gray-500 cursor-pointer"
+          onClick={handleSearch}
+        />
+      </div>
+      <div className="flex align-center justify-center w-full gap-8">
+        <div className="flex justify-center gap-2">
+          <Icon
+            icon="iconamoon:profile"
+            width="24"
+            height="24"
+            className="self-center"
+          />
+          <div className="flex flex-col gap-0 m-0 leading-none self-center">
+            <p className="m-0 text-sm">Welcome!</p>
+            {user.user ? (
+              <p className="m-0 font-semibold">{user.user?.username}</p>
+            ) : (
+              <p className="m-0 font-semibold">Please login</p>
+            )}
+          </div>
+        </div>
+        <Icon
+          icon="lineicons:cart-2"
+          width="24"
+          height="24"
+          className="self-center"
+        />
       </div>
     </nav>
   );
