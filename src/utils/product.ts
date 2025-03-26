32d098ad95bad8744a6deca7f8383c4a5b8c400d
D33102 <dalai2547@gmail.com>
2025-03-27 00:32:55 +0700

    chore

--- src/utils/product.ts
+++ src/utils/product.ts
@@ -180,7 +180,6 @@ export const updateProduct = async (
         color: color,
         price: price,
         description: desc,
-        sellerID: userId,
         imageURL: '',
         createdAt: new Date().toISOString(),
       },
